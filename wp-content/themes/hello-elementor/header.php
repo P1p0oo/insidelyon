d6808ebdd9c1b9c3134d70b56788aa2920f7c1be
MathiasEmile <mathias@eid-lab.com>
2022-04-26 11:58:55 +0200

    cookie desktop+landing pages

--- wp-content/themes/hello-elementor/header.php
+++ wp-content/themes/hello-elementor/header.php
@@ -19,9 +19,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<meta name="viewport" content="<?php echo esc_attr( $viewport_content ); ?>">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
+	<!-- Google Tag Manager -->
+	<script>
+	function getCookie(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return JSON.parse(c.substring(nameEQ.length,c.length));
+		}
+		return null;
+	}
+
+
+		(function(w,d,s,l,i){
+			const rgpd = getCookie("rgpd")
+			if(!rgpd || rgpd.gtm === false) return
+			w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','GTM-5J22RVN');
+	</script>
+	<!-- End Google Tag Manager -->
 </head>
 <body <?php body_class(); ?>>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5J22RVN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <?php
 hello_elementor_body_open();
 
